b2e3a79f8b7adf94477b2bdd14e6b10b6d1264e6
1whatleytay <1whatleytay@hdsb.ca>
2018-12-08 13:44:18 -0500

    First iBeacon setup

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { DeviceEventEmitter, SafeAreaView, Text } from 'react-native';
+import Beacons from 'react-native-beacons-manager';
+
+const regions = [
+    {
+        identifier: 'Estimotes',
+        uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
+    }
+];
 
 export default class PayBus extends Component {
+    constructor(props) {
+        super(props);
+
+        Beacons.requestWhenInUseAuthorization();
+        
+        regions.forEach(region => {
+            Beacons.startMonitoringForRegion(region);
+            Beacons.startRangingBeaconsInRegion(region);
+        });
+
+        Beacons.startUpdatingLocation();
+
+        this.subscription = DeviceEventEmitter.addListener('beaconsDidRange', data => {
+            console.log("Regions: " + data);
+        });
+    }
+
     render() {
         return (
             <SafeAreaView style={{ flex: 1 }}>
-                <Text style={{ alignItems: "center" }}>Hello World.</Text>
+                <Text style={{ flex: 1, alignItems: "center" }}>Hello World.</Text>
             </SafeAreaView>
         )
-        }
     }
+}
