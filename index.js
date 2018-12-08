13c5107bdc2b5a923c8db1a1ebed93b690adcab6
1whatleytay <1whatleytay@hdsb.ca>
2018-12-08 12:36:55 -0500

    Blank app.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
+import { AppRegistry } from 'react-native';
+import { PayBusApp } from 'App.js';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => PayBusApp);
