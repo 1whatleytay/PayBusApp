91aa29db3420e03f2ffe885b209e2e44332c2a59
1whatleytay <1whatleytay@hdsb.ca>
2018-12-08 16:05:41 -0500

    Added basic region detection

--- App.js
+++ App.js
@@ -1,56 +1,6 @@
-import React, { Component } from 'react';
-import { DeviceEventEmitter, SafeAreaView, Text } from 'react-native';
-import Beacons from 'react-native-beacons-manager';
-
-const regions = [
-    {
-        identifier: 'Estimotes',
-        uuid: 'B9407F30-F5F8-466E-AFF9-25556B57FE6D'
-    }
-];
-
-const stylesheet = {
-    flex:1
-}
-
-
-export default class PayBus extends Component {
-    constructor(props) {
-        super(props);
-
-        Beacons.requestWhenInUseAuthorization();
-        
-        regions.forEach(region => {
-            Beacons.startMonitoringForRegion(region);
-            Beacons.startRangingBeaconsInRegion(region);
-        });
-
-        Beacons.startUpdatingLocation();
-
-        this.subscription = DeviceEventEmitter.addListener('beaconsDidRange', data => {
-            console.log("Regions: " + data);
-        });
-    }
-
-    render() {
-        return (
-            <SafeAreaView style={{ flex: 1 }}>
-                <Text style={{ flex: 1, alignItems: "center" }}>Hello World.</Text>
-            </SafeAreaView>
-        )
-    }
-}
-
-import {
-    createStackNavigator,
-    createAppContainer
-} from 'react-navigation';
-
 import RouteOption from "./ReactNativeViews/RouteOptions";
 import ProfileScreen from "./ReactNativeViews/ProfileScreen";
 import HomeScreen from "./ReactNativeViews/HomeScreen";
-
-
 
 const RootStack = createStackNavigator({
     Home: {
--- ReactNativeViews/HomeScreen.js
+++ ReactNativeViews/HomeScreen.js
@@ -1,14 +1,10 @@
-import React, {
-    Component
-} from "react";
-import {
-    StyleSheet,
-    ScrollView,
-    View,
-    Text, Button
-}
-from "react-native";
+// React
+import React, { Component } from "react";
+import { StyleSheet, View, Text, Button } from "react-native";
 
+// Beacons Manager
+import Beacons from 'react-native-beacons-manager';
+import { DeviceEventEmitter } from 'react-native';
 
 const BalanceStyleSheet = StyleSheet.create({
     button:{
@@ -34,6 +30,8 @@ class CircleBalanceView extends Component {
 class BalanceSheet extends Component {
     constructor(props){
         super(props)
+
+        Beacons.request
     }
     render(){
         return(
@@ -44,25 +42,50 @@ class BalanceSheet extends Component {
     }
 }
 
+const regions = [
+    {
+        identifier: "Raspberry Pi",
+        uuid: "0D5C6FAD-6425-40E9-A106-228BAC3CB732"
+    }
+];
+
 class HomeScreen extends Component {
     static navigationOptions = {
         title: 'Welcome',
     };
+
     constructor(props) {
         super(props)
+
+        Beacons.requestAlwaysAuthorization()
+        Beacons.getAuthorizationStatus()
+
+        for (var region in regions) {
+            Beacons.startMonitoringForRegion(region)
+            Beacons.startRangingBeaconsInRegion(region)
+        }
+
+        Beacons.startUpdatingLocation()
+
+        this.listener = DeviceEventEmitter.addListener('beaconsDidRange', data => {
+            for (var region in regions) {
+                if (region.uuid === data.region.uuid) {
+                    alert("Detected Device: " + region.uuid)
+                }
+            }
+        })
     }
     render() {
-    const {
-        navigate
-    } = this.props.navigation;
-    return (
-        <View>
-          <BalanceSheet></BalanceSheet>
-          <View style={BalanceStyleSheet.Navigator}>
-              <Button title="Go to saved routes" onPress={() => navigate('Profile', {name:"jen"})}></Button>
-              <Button title="Goto " onPress={() => navigate('Profile', {name:"jen"})}></Button>
-          </View>
-        </View>
+        const { navigate } = this.props.navigation;
+
+        return (
+            <View>
+            <BalanceSheet></BalanceSheet>
+            <View style={BalanceStyleSheet.Navigator}>
+                <Button title="Go to saved routes" onPress={() => navigate('Profile', {name:"jen"})}></Button>
+                <Button title="Goto " onPress={() => navigate('Profile', {name:"jen"})}></Button>
+            </View>
+            </View>
         )
     }
 }
