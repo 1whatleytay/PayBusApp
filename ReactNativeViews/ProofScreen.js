468c78e76270cca98efce7d0e10cd9162f57abc4
1whatleytay <1whatleytay@hdsb.ca>
2018-12-09 11:51:53 -0500

    Proof Screen changes didn’t push T_T

--- ReactNativeViews/ProofScreen.js
+++ ReactNativeViews/ProofScreen.js
@@ -8,12 +8,12 @@ export default class ProofScreen extends Component {
         return (
             <View>
                 <PersonalData></PersonalData>
-                <View style={{ backgroundColor: "yellow", flex: 1 }}>
-                    <Text>
+                <View style={{ backgroundColor: "yellow", height: 500 }}>
+                    <Text style={{ fontSize: 25 }}>
                         You have authorized a payment of $
                             { (this.props.navigation.state["params"]["money"] / 100).toFixed(2) }.
                     </Text>
-                    <Text>
+                    <Text style={{ fontSize: 25 }}>
                         You will be going from { this.props.navigation.state["params"]["entry"] }
                             to { this.props.navigation.state["params"]["exist"] }.
                     </Text>
