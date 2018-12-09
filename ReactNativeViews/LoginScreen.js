259bde8b2173c5b28f4c215d1dcb076fdb89d743
1whatleytay <1whatleytay@hdsb.ca>
2018-12-08 22:47:11 -0500

    New Login Screen

--- ReactNativeViews/LoginScreen.js
+++ ReactNativeViews/LoginScreen.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
-import axios from 'axios';
+import { StyleSheet, View, Text, AsyncStorage, Button } from 'react-native'
+
+import { TextField } from 'react-native-material-textfield'
+
+import axios from 'axios'
 
 const loginStyle = StyleSheet.create({
-    
+    header: {
+        fontSize: 25,
+        fontWeight: "bold",
+        textAlign: "center"
+    }
 });
 
 export default class LoginScreen extends Component {
@@ -11,35 +18,36 @@ export default class LoginScreen extends Component {
         title: 'Alliboard Login',
     };
 
-    componentDidMount() {
-        console.log("Yep")
-        this.handleLogin('foo', 'bar')
-    }
-
     constructor(props) {
         super(props)
+
+        this.state = { username: "", password: "" }
     }
 
     render() {
         return (
             <View>
-                <Text></Text>
+                <TextField label="Email" keyboardType="email-address"
+                    onChangeText={username => this.setState({username})}></TextField>
+                <TextField label="Password" secureTextEntry="true"
+                    onChangeText={password => this.setState({password})}></TextField>
+                <Button title="Login" style={{ alignSelf: "center" }}
+                    onPress={() => this.handleLogin(this.state.username, this.state.password)}>Login</Button>
             </View>
         )
     }
 
-    async handleLogin(username, password) {
-        try {
-            await axios.post('https://api.alliboard.com/oauth/token', {
-                grant_type: 'password',
-                client_id: 1,
-                client_secret: 'gmJDSvVRjideHVIFupRiOceTFgZuyW3U0Fi5vbRj',
-                username,
-                password,
-                scope: ''
-            })
-        } catch (e) {
-            console.error(e.response.data)
-        }
+    handleLogin(username, password) {
+        axios.post('https://api.alliboard.com/oauth/token', {
+            grant_type: 'password',
+            client_id: 1,
+            client_secret: 'gmJDSvVRjideHVIFupRiOceTFgZuyW3U0Fi5vbRj',
+            username,
+            password,
+            scope: ''
+        }).then(response => {
+            AsyncStorage.setItem('token', response.data.access_token).then(
+                () => this.props.navigation.navigate('Home'))
+        }).catch(() => alert('Failed to login! Please try again...'))
     }
 }
