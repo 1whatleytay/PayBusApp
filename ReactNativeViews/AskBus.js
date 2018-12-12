21ab17f42c0a1b8d3ac5354abe112bce8ee1c023
Viktor Korolyuk <viktor.korolyuk@gmail.com>
2018-12-12 18:01:45 -0500

    Changing styles and adding debug-friendly options
    AskBus and ProofScreen have been updated with styles that follow the theme of the application.
    
    ProofScreen and AskBus did not behave properly when running early as they rely on data passed from another view. Error handlers were added to remedy the issue.

--- ReactNativeViews/AskBus.js
+++ ReactNativeViews/AskBus.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
-import { View, Text, AsyncStorage } from 'react-native'
+import { View, Text, AsyncStorage, StyleSheet } from 'react-native'
 
 import { RaisedTextButton } from 'react-native-material-buttons'
 
 import axios from 'axios'
+const ButtonStyle = StyleSheet.create({
+  NormalButton: {
+    flex: 1,
+    margin: 10,
+    height: 100,
+    backgroundColor: "#222",
+    borderRadius: 0,
+
+  },
+  ButtonYes: {
+    backgroundColor: "#00ADB5"
+  },
+  ButtonNo: {
+    backgroundColor: "#222"
+  },
+});
 
 export default class AskBus extends Component {
     async checkRoutes() {
         let token = await AsyncStorage.getItem("token")
+        let uuid = null
 
-        let uuid = this.props.navigation.state["params"]["uuid"];
+        try {
+          uuid = this.props.navigation.state["params"]["uuid"];
+        } catch (e) {
+          console.warn("Skipped as this.props.navigation.state.params was not found.")
+        }
         // axios.post('https://api.alliboard.com/trips/', {uuid}, {
         //     headers: { authorization: "Bearer " + token }
         // }).then((response) => {
@@ -17,9 +38,11 @@ export default class AskBus extends Component {
         //     this.props.navigation.replace("Proof", { money: response.cost,
         //         entry: "St John's Go Station", exit: "Central Stop"})
         // }).catch(console.error)
+
         this.props.navigation.replace("Proof", { money: 250,
             entry: "St John's Go Station", exit: "Central Stop"})
     }
+
 
     render() {
         return (
@@ -27,8 +50,8 @@ export default class AskBus extends Component {
                 <Text style={{ textAlign: "center", fontSize: 40, marginTop: 50 }}>
                     Are you getting on the bus?</Text>
                 <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around', marginTop: 50 }}>
-                    <RaisedTextButton title="Yes" onPress={ () => this.checkRoutes() }>Yes</RaisedTextButton>
-                    <RaisedTextButton title="No" onPress={ () => this.props.navigation.goBack() }>No</RaisedTextButton>
+                    <RaisedTextButton style={[ButtonStyle.NormalButton, ButtonStyle.ButtonYes]} titleColor="rgb(255,255,255)" title="Yes"  onPress={ () => this.checkRoutes() }>Yes</RaisedTextButton>
+                    <RaisedTextButton  style={[ButtonStyle.NormalButton, ButtonStyle.ButtonNo]} titleColor="rgb(255,255,255)" title="No" onPress={ () => this.props.navigation.goBack() }>No</RaisedTextButton>
                 </View>
             </View>
         )
--- ReactNativeViews/ProofScreen.js
+++ ReactNativeViews/ProofScreen.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 import PersonalData from './res/PersonalData'
+const ProofStyles = StyleSheet.create({
+  Text: {
+    justifyContent: "center",
+    margin: 15,
+    marginBottom: 0,
+    marginTop: 12,
+    padding: 15,
+
+    borderRadius: 4,
+    fontSize: 25,
+
+    color: "white",
+    backgroundColor: "#02C39A",
+  }
+});
 
 export default class ProofScreen extends Component {
     render() {
+        let input1 =this.props.navigation.state["params"] ? (this.props.navigation.state["params"]["money"] / 100).toFixed(2) : "";
+        let entry_exit = this.props.navigation.state["params"] ?this.props.navigation.state["params"]["entry"] + " to " + this.props.navigation.state["params"]["exit"] : "";
         return (
             <View>
                 <PersonalData></PersonalData>
                 <View style={{ height: 500 }}>
-                    <Text style={{ backgroundColor: "#02C39A", fontSize: 25, justifyContent: "center", borderRadius: 20, margin: 15, padding: 15 }}>
-                        You have authorized a payment of $
-                            { (this.props.navigation.state["params"]["money"] / 100).toFixed(2) }.
+                    <Text style={ProofStyles.Text}>
+                        You have authorized a payment of ${input1}.
                     </Text>
-                    <Text style={{ backgroundColor: "#02C39A", fontSize: 25, justifyContent: "center", borderRadius: 20, margin: 15, padding: 15 }}>
-                        You will be going from { this.props.navigation.state["params"]["entry"] } to { this.props.navigation.state["params"]["exit"] }.
+                    <Text style={ProofStyles.Text}>
+                        You will be going from {entry_exit}.
                     </Text>
                 </View>
             </View>
